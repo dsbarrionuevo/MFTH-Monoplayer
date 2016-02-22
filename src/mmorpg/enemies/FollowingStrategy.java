5a9d9f20cbe73399fa17955f96ae63ea601518aa
Diego Barrionuevo <dsbarrionuevo@hotmail.com>
2016-02-22 12:19:33 -0300

    Algoritmo básico de seguimiento completo.

--- src/mmorpg/enemies/FollowingStrategy.java
+++ src/mmorpg/enemies/FollowingStrategy.java
@@ -45,7 +45,6 @@ public abstract class FollowingStrategy {
         if (target != null) {
             //no hacerlo a cada instante...
             Room room = chaser.getRoom();
-
             Vector2f originPosition = room.getOriginPosition();
             Vector2f targetPosition = new Vector2f(target.getPosition().x - originPosition.x + target.getWidth() / 2, target.getPosition().y - originPosition.y + target.getHeight() / 2);
             Vector2f chaserPosition = new Vector2f(chaser.getPosition().x - originPosition.x + chaser.getWidth() / 2, chaser.getPosition().y - originPosition.y + chaser.getHeight() / 2);
@@ -64,17 +63,11 @@ public abstract class FollowingStrategy {
     }
 
     public void follow(GameContainer gc, int delta) {
-//        Vector2f targetPosition = new Vector2f(target.getPosition().x + target.getWidth() / 2, target.getPosition().y + target.getHeight() / 2);
-//        Vector2f chaserPosition = new Vector2f(chaser.getPosition().x + chaser.getWidth() / 2, chaser.getPosition().y + chaser.getHeight() / 2);
-//        float distance = (float) Math.sqrt((targetPosition.x - chaserPosition.x) * (targetPosition.x - chaserPosition.x) - (targetPosition.y - chaserPosition.y) * (targetPosition.y - chaserPosition.y));
-//        int direction = Room.DIRECTION_WEST;
         float moveFactor = 7f * (delta / 100f);
         Room room = chaser.getRoom();
         Tile tileTarget = room.getCurrentTile(target);
         Tile tileChaser = room.getCurrentTile(chaser);
-        
-            System.out.println(tileTarget.getId());
-            System.out.println(tileChaser.getId());
+        //first chase in horizontal
         if (tileTarget.getTileX() < tileChaser.getTileX()) {
             if (room.canMoveTo(chaser, Room.DIRECTION_WEST)) {
                 chaser.getPosition().x -= moveFactor;
@@ -84,6 +77,7 @@ public abstract class FollowingStrategy {
                 chaser.getPosition().x += moveFactor;
             }
         } else if (tileTarget.getTileX() == tileChaser.getTileX()) {
+            //then chase in vertical
             if (tileTarget.getTileY() < tileChaser.getTileY()) {
                 if (room.canMoveTo(chaser, Room.DIRECTION_NORTH)) {
                     chaser.getPosition().y -= moveFactor;
@@ -92,13 +86,18 @@ public abstract class FollowingStrategy {
                 if (room.canMoveTo(chaser, Room.DIRECTION_SOUTH)) {
                     chaser.getPosition().y += moveFactor;
                 }
-            } else if (tileTarget.getTileY() == tileChaser.getTileY()) {
-                //chaser.stopFollowing();
             }
         }
         if (tileTarget.getTileX() == tileChaser.getTileX() && (tileTarget.getTileY() == tileChaser.getTileY())) {
             //chaser.stopFollowing();
-            System.out.println("FOUND");
+            Vector2f targetPosition = new Vector2f(target.getPosition().x + target.getWidth() / 2, target.getPosition().y + target.getHeight() / 2);
+            Vector2f chaserPosition = new Vector2f(chaser.getPosition().x + chaser.getWidth() / 2, chaser.getPosition().y + chaser.getHeight() / 2);
+            float distance = (float) Math.sqrt((targetPosition.x - chaserPosition.x) * (targetPosition.x - chaserPosition.x) - (targetPosition.y - chaserPosition.y) * (targetPosition.y - chaserPosition.y));
+            if (distance > 5) {
+                double angle = Math.atan2(targetPosition.y - chaserPosition.y, targetPosition.x - chaserPosition.x);
+                chaser.getPosition().x += Math.cos(angle) * moveFactor;
+                chaser.getPosition().y += Math.sin(angle) * moveFactor;
+            }
         }
     }
 
